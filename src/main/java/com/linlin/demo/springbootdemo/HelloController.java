71ff14807b45f164cd512d4843e13c2d800244c4
wu.zeheng <wu.zeheng@vpclub.cn>
2019-09-25 09:59:40 +0800

    first

---
+++ src/main/java/com/linlin/demo/springbootdemo/HelloController.java
@@ -0,0 +1,19 @@
+package com.linlin.demo.springbootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Desc
+ * @Author zh wu
+ * @Date 2019/8/24
+ */
+@RestController
+//@RequestMapping("/oms/open/flightcollab/flightinfo")
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hehe";
+    }
+
+}
