7092914b111bd594e0b1da8a5f835987f4030900
wu.zeheng <wu.zeheng@vpclub.cn>
2019-10-24 17:15:24 +0800

    增加jar包修改测试

--- src/main/java/com/linlin/demo/springbootdemo/HelloController.java
+++ src/main/java/com/linlin/demo/springbootdemo/HelloController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
-        return "hehe";
+        return "heheDa!";
     }
 
 }
